4e0966e55aaf499693a71d277f0b35255cd9d5de
Esther74 <estherhache@hotmail.com>
2021-11-02 12:16:34 +0100

    schema y data

---
+++ src/main/resources/schema-postgres.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  soporte
+ * Created: 2 nov. 2021
+ */
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities(id serial PRIMARY KEY, name VARCHAR(255), population integer);
