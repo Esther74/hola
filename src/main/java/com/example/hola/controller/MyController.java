0cbea7724f3b2ad42a0832445bf987b26202b24f
Esther74 <estherhache@hotmail.com>
2021-11-03 08:35:13 +0100

    controller como json

--- src/main/java/com/example/hola/controller/MyController.java
+++ src/main/java/com/example/hola/controller/MyController.java
@@ -9,30 +9,32 @@ import com.example.hola.model.City;
 import com.example.hola.service.ICityService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author soporte
  */
 
-//@RestController
-@Controller
+@RestController
+//@Controller
 public class MyController {
     
     @Autowired
     private ICityService cityService;
     
     @GetMapping("/showCities")
-    //public List<City> findCities(){ //como JSON
-    public String findCities(Model model){
+    public List<City> findCities(){ //como JSON
+    
+    //public String findCities(Model model){    
         //return cityService.findAll(); //como JSON
-        List<City> cities = cityService.findAll();
-        model.addAttribute("cities", cities);
-        //return cities;
-        return "showCities";
+        List<City> cities = cityService.findAll(); //JSON
+        //model.addAttribute("cities", cities);
+        return cities;
+        //return "showCities";
     }
     //http://localhost:8080/showCities
 }
